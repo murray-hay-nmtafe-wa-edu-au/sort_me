7a335fc2f2e52a4c293728edda2b1250549896db
Tejkin <Thomas1467@hotmail.com>
2023-03-31 14:19:46 +0800

    Added sortList function

--- unsorted.py
+++ unsorted.py
@@ -2,12 +2,27 @@ import random
 
 unsorted = []
 
-for i in range (100):
+for i in range (4):
     x = random.randint(0,500)
     unsorted.append(x)
 print(unsorted)
 
-unsorted.sort()
-print(unsorted)
 
-#
+def sortList(lst):
+    """Takes in a list of unsorted numbers and orders them from smallest to biggest
+
+    Args:
+        lst (list): a list of numbers (int)
+        
+    Return:
+        lst (list): prints a sorted list
+    """
+    listLength = len(lst)
+    
+    for i in range(listLength):
+        for b in range(listLength - i - 1): #j is the total amount of comparisons made
+            if lst[b] > lst[b + 1]:
+                lst[b], lst[b+1] = lst[b+1], lst[b]
+    print(lst)
+
+sortList(unsorted)
