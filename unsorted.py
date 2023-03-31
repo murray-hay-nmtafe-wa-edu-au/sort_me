a500888a1fa58916425773512dac5e8d9f847603
V125630 <v125630@tafe.wa.edu.au>
2023-03-31 15:18:21 +0800

    commit

--- unsorted.py
+++ unsorted.py
@@ -7,7 +7,13 @@ for i in range (100):
     unsorted.append(x)
 print(unsorted)
 
-unsorted.sort()
+#unsorted.sort()
+#print(unsorted)
+for num in range(len(unsorted)):
+    for rnum in range(len(unsorted)- 1):
+        if unsorted[num] > unsorted[rnum]:
+            x = unsorted[num] 
+            y = unsorted[rnum] 
+            unsorted[num] = y
+            unsorted[rnum] = x
 print(unsorted)
-
-#
