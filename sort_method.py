c76449ad6ddc846888e761f3545023f52b6da4b5
BradenPrograms <124344218+bradenwatson@users.noreply.github.com>
2023-03-31 20:43:46 +0800

    Update sort_method.py
    - Made list_length simpler.
    - Add bubble sort method.

--- sort_method.py
+++ sort_method.py
@@ -1,6 +1,12 @@
-def sort(input_list):
-    list_length = 0
-
-    # Retrieve number of items in list
+def sort(input_list: list):
     list_length = len(input_list)
 
+    for counter in range(list_length):
+        for index in range(0, list_length-counter-1):
+            # Swap elements if they are in the wrong order.
+            if input_list[index] > input_list[index+1]:
+                input_list[index], input_list[index+1] = input_list[index+1], input_list[index]
+    
+    return input_list
+        
+        
