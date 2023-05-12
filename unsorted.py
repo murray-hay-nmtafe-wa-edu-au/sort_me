adf567afe166d41e546e4a8864fde3d99bc0846f
prgrobots <91106994+prgrobots@users.noreply.github.com>
2023-05-12 14:44:49 +0800

    Update unsorted.py
    linting edit

--- unsorted.py
+++ unsorted.py
@@ -3,32 +3,32 @@ import time
 
 unsorted = []
 
-for i in range (100):
-    x = random.randint(0,500)
-    unsorted.append(x)
-print("unsorted: ", unsorted)
+for i in range(100):
+    unsorted_list = random.randint(0, 500)
+    append = unsorted.append(unsorted_list)
+print(unsorted)
 
 
-def bubbleSortList(lst):
-    start_time = time.time_ns()/1000
+def bubble_sort_list(lst):
+    start_time = time.time_ns() / 1000
     """Takes in a list of unsorted numbers and orders them from smallest to biggest
 
     Args:
-        lst (list): a list of numbers (int)
-        
+          lst (list): a list of numbers (int)
+
     Return:
         lst (list): prints a sorted list
     """
-    listLength = len(lst)
-    
-    for i in range(listLength):
-        for b in range(listLength - i - 1): #b is the total amount of comparisons made
+    list_length = len(lst)
+
+    for bitter in range(list_length):
+        for b in range(list_length - bitter - 1):  # b is the total amount of comparisons made
             if lst[b] > lst[b + 1]:
-                lst[b], lst[b+1] = lst[b+1], lst[b]
+                lst[b], lst[b + 1] = lst[b + 1], lst[b]
     print(lst)
-    end_time = time.time_ns()/1000
+    end_time = time.time_ns() / 1000
     elapsed_time = (end_time - start_time)
     print(f"Bubble sort elapsed time: {elapsed_time}ms")
-    
-if __name__ == '__main__':
-    bubbleSortList(unsorted)
+
+
+bubble_sort_list(unsorted)
